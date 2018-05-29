b022bd780342d0b0db7a3a3a33bb86d044086abc
jCzachor6 <jakubczachor7@gmail.com>
2018-05-29 17:04:50 +0200

    Added getContactById(Long id) to ContactController

--- src/main/java/com/czachor/jakub/pgs/projekt/contacts/controller/ContactController.java
+++ src/main/java/com/czachor/jakub/pgs/projekt/contacts/controller/ContactController.java
@@ -1,4 +1,34 @@
 package com.czachor.jakub.pgs.projekt.contacts.controller;
 
+import com.czachor.jakub.pgs.projekt.contacts.models.ContactRes;
+import com.czachor.jakub.pgs.projekt.contacts.service.ContactService;
+import com.czachor.jakub.pgs.projekt.contacts.service.exceptions.ContactDoesNotExistException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/contact")
 public class ContactController {
+
+    private final ContactService contactService;
+
+    @Autowired
+    public ContactController(ContactService contactService) {
+        this.contactService = contactService;
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<ContactRes> getContactById(@PathVariable("id") Long id){
+        try{
+            ContactRes contactRes = contactService.findContactById(id);
+            return new ResponseEntity<>(contactRes, HttpStatus.FOUND);
+        }catch(ContactDoesNotExistException e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
---
+++ src/test/java/models/ContactControllerTest.java
@@ -0,0 +1,99 @@
+package models;
+
+import com.czachor.jakub.pgs.projekt.contacts.controller.ContactController;
+import com.czachor.jakub.pgs.projekt.contacts.models.asm.ContactAsm;
+import com.czachor.jakub.pgs.projekt.contacts.models.entities.Contact;
+import com.czachor.jakub.pgs.projekt.contacts.service.ContactService;
+import com.czachor.jakub.pgs.projekt.contacts.service.exceptions.ContactDoesNotExistException;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.StringContains.containsString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class ContactControllerTest {
+    @InjectMocks
+    private ContactController controller;
+
+    @Mock
+    private ContactService service;
+
+    private MockMvc mockMvc;
+
+    private Contact contact;
+
+    @Before
+    public void setup(){
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+        contact = new Contact();
+        contact.setId(1L);
+        contact.setName("Name");
+        contact.setSurname("Surname");
+        contact.setAddress("Contact Address");
+        contact.setPhoneNumber("100 200 300");
+    }
+
+    @Test
+    public void findContactByIdTest() throws Exception {
+        when(service.findContactById(1L)).thenReturn(new ContactAsm().toResource(contact));
+        mockMvc.perform(get("/contact/{id}", 1L))
+                .andExpect(status().isFound())
+                .andDo(print())
+                .andExpect(jsonPath("$.name", is("Name")))
+                .andExpect(jsonPath("$.surname", is("Surname")))
+                .andExpect(jsonPath("$.address", is("Contact Address")))
+                .andExpect(jsonPath("$.phoneNumber", is("100 200 300")))
+                .andExpect(jsonPath("$.rid", is(1)))
+                .andExpect(jsonPath("$.links").isArray())
+                .andExpect(jsonPath("$.links[0].href", containsString("/contact/1")));
+    }
+
+    @Test
+    public void findNonExistingContactTest() throws Exception{
+        when(service.findContactById(1L)).thenThrow(new ContactDoesNotExistException());
+        mockMvc.perform(get("/contact/{id}", 1L))
+                .andExpect(status().isNotFound());
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
