c1d01f2be438f1624a8dde830cd995edea178342
Jakub Czachor <jakubczachor7@gmail.com>
2018-08-24 10:21:16 +0200

    Small changes (#10)
    * Changed ContractController from @Controller to @RestController
    
    * Added AppControllerAdvice
    
    * Removed useless frontend files.
    
    * Removed useless backend lines of code.

--- src/main/java/czachor/jakub/contacts/config/AppConfig.java
+++
@@ -1,8 +0,0 @@
-package czachor.jakub.contacts.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-
-@Configuration
-public class AppConfig {
-}
--- src/main/java/czachor/jakub/contacts/controller/ContactController.java
+++ src/main/java/czachor/jakub/contacts/controller/ContactController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,7 +15,7 @@ import java.util.List;
  * @author Jakub Czachor
  * ContactController consist all REST methods used to manage contacts.
  */
-@Controller
+@RestController
 @RequestMapping("/contact")
 public class ContactController {
 
@@ -33,21 +30,17 @@ public class ContactController {
      * @param id - Unique identifier of a Comment
      * @return ContactDTO of given id
      */
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public ResponseEntity<ContactDTO> getContactById(@PathVariable("id") Long id){
-        try{
-            ContactDTO contactDTO = contactService.findContactById(id);
-            return new ResponseEntity<>(contactDTO, HttpStatus.FOUND);
-        }catch(ContactDoesNotExistException e){
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ContactDTO> getContactById(@PathVariable("id") Long id) {
+        ContactDTO contactDTO = contactService.findContactById(id);
+        return new ResponseEntity<>(contactDTO, HttpStatus.FOUND);
     }
 
     /**
      * @return List of all contacts.
      */
-    @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<ContactDTO>> getAllContacts(){
+    @GetMapping
+    public ResponseEntity<List<ContactDTO>> getAllContacts() {
         List<ContactDTO> contactDTOList = contactService.findAllContacts();
         return new ResponseEntity<>(contactDTOList, HttpStatus.OK);
     }
@@ -56,41 +49,29 @@ public class ContactController {
      * @param contactDTO - resource to delete from database
      * @return HttpStatus OK if deleted, NOT_FOUND if couldn't find entity to delete
      */
-    @RequestMapping(method = RequestMethod.DELETE)
-    public ResponseEntity deleteContact(@RequestBody ContactDTO contactDTO){
-        try{
-            contactService.deleteContact(contactDTO);
-            return new ResponseEntity(HttpStatus.OK);
-        }catch(ContactDoesNotExistException e){
-            return new ResponseEntity(HttpStatus.NOT_FOUND);
-        }
+    @DeleteMapping
+    public ResponseEntity deleteContact(@RequestBody ContactDTO contactDTO) {
+        contactService.deleteContact(contactDTO);
+        return new ResponseEntity(HttpStatus.OK);
     }
 
     /**
      * @param contactDTO - resource to add to database
      * @return HttpStatus CREATED if added, NOT_FOUND if couldn't get entity back from database.
      */
-    @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<ContactDTO> createContact(@RequestBody ContactDTO contactDTO){
-        try {
-            contactService.addContact(contactDTO);
-            return new ResponseEntity<>(HttpStatus.CREATED);
-        }catch (ContactDoesNotExistException e){
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+    @PostMapping
+    public ResponseEntity<ContactDTO> createContact(@RequestBody ContactDTO contactDTO) {
+        contactService.addContact(contactDTO);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     /**
      * @param contactDTO - resouce to update in database
      * @return HttpStatus OK if edited, NOT_FOUND if couldn't find entity to edit
      */
-    @RequestMapping(method = RequestMethod.PUT)
-    public ResponseEntity<ContactDTO> updateContact(@RequestBody ContactDTO contactDTO){
-        try {
-            ContactDTO updated = contactService.editContact(contactDTO);
-            return new ResponseEntity<>(updated, HttpStatus.OK);
-        }catch(ContactDoesNotExistException e){
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+    @PutMapping
+    public ResponseEntity<ContactDTO> updateContact(@RequestBody ContactDTO contactDTO) {
+        ContactDTO updated = contactService.editContact(contactDTO);
+        return new ResponseEntity<>(updated, HttpStatus.OK);
     }
 }
